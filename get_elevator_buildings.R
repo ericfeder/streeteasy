eb919bc91fd49e527473436e944e5e1435135bb9
ef2240 <efeder@2u.com>
2016-12-05 15:18:26 -0500

    use proper street name for elevator buildings

--- get_elevator_buildings.R
+++ get_elevator_buildings.R
@@ -18,19 +18,21 @@ getAddressInfo <- function(house_number, street, app_id = Sys.getenv("NYC_API_ID
   url <- URLencode(sprintf(base_url, house_number, street, app_id, app_key))
   message(sprintf("Hitting %s", url))
   response <- content(GET(url))
-  return(list(street_name_normalized = response$address$firstStreetNameNormalized,
+  return(list(street_name_normalized = response$address$giStreetName1,
               zip_code = response$address$zipCode))
 }
 tryAddressInfo <- function(house_number, street){
   try(getAddressInfo(house_number, street))
 }
 address_info <- mapply(FUN = tryAddressInfo, elevators$HOUSE_NUMBER, elevators$STREET_NAME, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+address_found <- sapply(address_info, function(x) !is.null(x$street_name_normalized))
 
 # Write to csv
 elevators_write <- elevators %>%
+  filter(address_found) %>%
   transmute(house_number = HOUSE_NUMBER,
-            street_name = sapply(address_info, function(x) x$street_name_normalized),
-            zip_code = as.character(sapply(address_info, function(x) x$zip_code)),
+            street_name = sapply(address_info[address_found], function(x) x$street_name_normalized),
+            zip_code = as.character(sapply(address_info[address_found], function(x) x$zip_code)),
             zip_code = ifelse(zip_code == "NULL", NA, zip_code)) %>%
   arrange(street_name, as.integer(house_number))
 write.csv(elevators_write, file = "manhattan_elevator_buildings.csv", row.names = FALSE)
